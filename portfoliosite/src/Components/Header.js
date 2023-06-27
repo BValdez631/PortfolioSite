b98fc5629069fe7da3c2499f1abcaf81be5554f4
Blas Valdez <blasvaldez631@gmail.com>
2023-06-27 15:38:43 -0700

    Organized file structure for app

---
+++ portfoliosite/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        <h1>Blas Valdez</h1>
+        <h2>Web Developer &#x2022; Network Administrator</h2>
+    </header>
+  )
+}
+
+export default Header
